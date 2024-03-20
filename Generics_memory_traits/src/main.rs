d8eae7ffe7152c38c0a33ed142c57350f9b519ae
Paul Hitchcox <142123149+eobardthawne21p@users.noreply.github.com>
2024-03-20 18:30:04 +0000

    added lecture notes 03/20/24

---
+++ Generics_memory_traits/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // // Create roles
+    // let user_role = Role::User;
+    // let admin_role = Role::Admin;
+
+    // // Create users with roles
+    // let users = [
+    //     (String::from("alice"), User { username: "alice".to_string(), role: user_role.clone() }),
+    //     (String::from("bob"), User { username: "bob".to_string(), role: admin_role.clone() }),
+    // ]
+    // .iter()
+    // .cloned()
+    // .collect::<HashMap<String, User>>();
+
+    // // Authenticate and authorize a user
+    // //let result = authenticate_and_authorize(&users, "alice", TagPrivilege::Write);
+    // let result = authenticate_and_authorize(&users, "bob", TagPrivilege::Write);
+
+    // match result {
+    //     Ok(_) => println!("User is authenticated and authorized"),
+    //     Err(e) => println!("Error: {}", e),
+    // }
+}
